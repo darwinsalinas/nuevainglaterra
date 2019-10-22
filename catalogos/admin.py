60751fa026b9b63ee212993ccf66732c16d1f8ef
Darwin <salinash2000@gmail.com>
2019-10-21 20:23:30 -0600

    register new models on the admin

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Marca, Modelo, Color, UnidadMedida, Presentacion, TipoArticulo, OrigenVenta, TipoFactura
+from .models import Marca, Modelo, Color, UnidadMedida, Presentacion, TipoArticulo, OrigenVenta, TipoFactura, Genero, \
+    EstadoCivil, Pais, Departamento, Municipio, Comunidad
 
 admin.site.register([
     Marca,
@@ -9,5 +10,11 @@ admin.site.register([
     Presentacion,
     TipoArticulo,
     OrigenVenta,
-    TipoFactura
+    TipoFactura,
+    Genero,
+    EstadoCivil,
+    Pais,
+    Departamento,
+    Municipio,
+    Comunidad,
 ])
