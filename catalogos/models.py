0251d56134842898e727e773a6b5e38aa03dffe5
Darwin <salinash2000@gmail.com>
2019-10-21 20:02:40 -0600

    catalogs models

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+
+
+class Modelo(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Modelo'
+        verbose_name_plural = 'Modelos'
+
+
+class Color(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Color'
+        verbose_name_plural = 'Color'
+
+
+class UnidadMedida(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Unidad de medida'
+        verbose_name_plural = 'Unidades de medida'
+
+
+class Presentacion(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Presentación'
+        verbose_name_plural = 'Presentaciones'
